cf20722f25aafc1e13661c35745a8679bdf09e68
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-02-19 11:51:34 +0100

    Remove unwanted configs

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     typedRoutes: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
